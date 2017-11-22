19950b3d7c7486f721c604ca74efef8fcb0e616e
DASM8 <DASM8@225451-WIN7.npfit.nhs.uk>
2017-11-22 16:19:55 +0000

    Update

---
+++ src/test/java/golftracker/mvc/GolfTrackerControllerTest.java
@@ -0,0 +1,31 @@
+package golftracker;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class GolfTrackerControllerTest {
+    
+    @InjectMocks GolfTrackerController controller;
+    
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks( this );
+        mockMvc = MockMvcBuilders.standaloneSetup( controller).build();
+    }
+
+    @Test
+    public void test() throws Exception{
+        mockMvc.perform( get("/test") ).andDo( print() );
+    }
+
+}
