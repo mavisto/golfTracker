d2e83e831ffa1c1e828e9329acc802553901b2d8
DASM8 <DASM8@225451-WIN7.npfit.nhs.uk>
2017-11-23 20:04:27 +0000

    Update

--- src/main/java/golftracker/rest/mvc/GolfTrackerController.java
+++ src/main/java/golftracker/rest/mvc/GolfTrackerController.java
@@ -1,11 +1,13 @@
 package golftracker.rest.mvc;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import golftracker.core.entities.Golftracker;
 import golftracker.core.services.GolftrackerService;
 import golftracker.rest.resources.GolftrackerResource;
 import golftracker.rest.resources.asm.GolftrackerResourceAsm;
@@ -24,8 +26,9 @@ public class GolfTrackerController {
         @PathVariable Long golftrackerId) {
         
         Golftracker golftracker = service.find( golftrackerId );
-        GolftrackerResource res = new GolftrackerResourceAsm().toreaource(golftracker);
+        GolftrackerResource res = new GolftrackerResourceAsm().toResource(golftracker);
         
+        return new ResponseEntity<GolftrackerResource>( res, HttpStatus.OK );
     }
 
 }
